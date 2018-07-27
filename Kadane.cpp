db720e64232918ff9a1a2320db578b0ef53a308c
PARAS GOYAL <gparas95@yahoo.com>
2018-07-27 02:07:31 -0700

    gfg 1

---
+++ Kadane.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int T, N;
+	cin>>T;
+	if(T<1 && T>200)
+		return -1;
+	while(T)
+	{
+		cin>>N;
+		if(N<1 && N>1000)
+			return -1;
+		for(int i=0; i<N; i++)
+		{
+			int A[N];
+		}
+		T--;
+	}
+	return 0;
+}
